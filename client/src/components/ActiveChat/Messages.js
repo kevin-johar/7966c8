25597c6da7af28359c6679ab929e7f78c89a3c96
Kevin Johar <kevin.s.johar@gmail.com>
2022-03-24 22:09:57 +0000

    feat: other User's last read status shown

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Avatar } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  readIconContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '100%'
+  },
+  readIcon: {
+    height: 15,
+    width: 15,
+    marginRight: 11,
+    marginTop: 6,
+  }
+}));
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const classes = useStyles();
+
+  const readIcon = () => 
+    <Box className={classes.readIconContainer}>
+      <Avatar
+        alt={otherUser.username}
+        src={otherUser.photoUrl}
+        className={classes.readIcon}
+      />
+    </Box>;
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
+        const canShow = message?.id === otherUser?.lastRead?.messageId;
+
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <Box key={message.id}>
+            <SenderBubble
+            text={message.text} 
+            time={time} />
+            {canShow && readIcon()}
+          </Box>
         ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
+          <Box key={message.id} >
+            <OtherUserBubble
+              text={message.text}
+              time={time}
+              otherUser={otherUser}
+            />
+            {canShow && readIcon()}
+          </Box>
         );
       })}
     </Box>
