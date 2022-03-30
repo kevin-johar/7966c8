8f60215e0b840697720ec9ddb4e4ad69b053de09
Atulpreet (Kevin) Johar <ajohar5@uwo.ca>
2022-03-29 23:37:01 -0400

    fix: authentication check added

--- server/messenger_backend/views/api/conversation_user.py
+++ server/messenger_backend/views/api/conversation_user.py
@@ -1,20 +1,31 @@
 from rest_framework.views import APIView
+from django.contrib.auth.middleware import get_user
 from messenger_backend.models import ConversationUser
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 
-class ConversationUserPair(APIView):
-    def put(self, request, conversationId, userId):
-        body = request.data
-        messageId = body.get("lastReadMessageId")
 
+class ConversationUserPair(APIView):
+    def put(self, request, conversation_id):
         try:
+            user = get_user(request)
+
+            if user.is_anonymous:
+                return HttpResponse(status=401)
+
+            body = request.data
+            message_id = body.get("lastReadMessageId")
+
+            user_id = user.id
+
             conversationUser, created = ConversationUser.objects.update_or_create(
-                conversation__id=conversationId,
-                user__id=userId,
-                defaults={'user_id': userId, 'conversation_id': conversationId, 'lastReadDate': timezone.now(), 'message_id': messageId}
+                conversation__id=conversation_id,
+                user__id=user_id,
+                defaults={'user_id': user_id, 'conversation_id': conversation_id, 'lastReadDate': timezone.now(),
+                          'message_id': message_id}
             )
-            return JsonResponse({"conversationId": conversationId, "userId": userId, "lastRead": { "date": conversationUser.lastReadDate, "messageId": messageId }})
+            return JsonResponse({"conversationId": conversation_id, "userId": user_id,
+                                 "lastRead": {"date": conversationUser.lastReadDate, "messageId": message_id}})
         except Exception as e:
             print('Something\'s going wrong: ', e)
             return HttpResponse(status=500)
